9eb2492bfd216e7428d5e7c1392698195a0ec12b
apgyapgy <2648226617@qq.com>
2019-09-23 17:39:22 +0800

    图片压缩，图表页完成30%

--- cloudfunctions/getChartData/index.js
+++ cloudfunctions/getChartData/index.js
@@ -10,14 +10,15 @@ const db = cloud.database();
 const $ = db.command.aggregate;
 // 云函数入口函数
 exports.main = async (event, context) => {
-  const wxContext = cloud.getWXContext();//event.userInfo.openId
   const _ = db.command;
   return await db.collection('bookList').aggregate().match({
-    _openid: event.userInfo.openId,
+    openid: event.userInfo.openId,
     amtType: event.amtType,
     // bookDate: $.and([$.gte(['$bookDate', event.beginTime]), $.lte(['$bookDate', event.endTime])])
     // bookDate: $.gte(event.beginTime)//.and(_.lte(event.endTime))
-    bookDate: { $gte: event.beginTime, $lte: event.endTime }
+    bookYear:event.year,
+    bookMonth:event.month
+    // bookDate: { $gte: event.beginTime, $lte: event.endTime }
   }).group({
     _id: {
       bookTypeName: '$bookTypeName',
--- miniprogram/pages/chart/chart.js
+++ miniprogram/pages/chart/chart.js
@@ -2,10 +2,55 @@ let app = getApp();
 Page({
   data: {
     footerIndex: 1,
+    amtType:'0',//0支出，1收入
+    amtTypeArr:['支出','收入'],
+    dateType:1,//时间范围1月，2年
+    chartList:[],
+    chartListCount:0
   },
   onLoad: function (options) {
   },
   onShow: function () {
+    let month = new Date().getMonth()+1;
+    let year = new Date().getFullYear();
+    this.initData(year,month);
+  },
+  initData(year,month){
+    let _this = this;
+    app.getAjax({
+      url:'getChartData',
+      params:{
+        amtType:_this.data.amtType,
+        year: year,
+        month:month
+      },
+      success(res){
+        console.log("getChartData:",res);
+        let data = res.result.list;
+        let count = 0;
+        for(var key in data){
+          data[key].count = parseInt(data[key].count*100)/100;
+          count += data[key].count;
+        }
+        _this.setData({
+          chartList:data,
+          chartListCount:count
+        });
+      }
+    });
+  },
+  amtTypeChange(e){//选择账单类型
+    this.setData({
+      amtType:e.detail.value
+    });
+  },
+  setDateType(e){//选择日期范围
+    let type = e.currentTarget.dataset.type;
+    if(type != this.data.dateType){
+      this.setData({
+        dateType:type
+      });
+    }
   },
   changeFooter(e) {
     app.changeFooter(e);
